1c174485c43e553fd283dc334022c530f79a384d
Frank J. Reashore <frank.reashore@gov.bc.ca>
2017-11-14 14:32:39 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const root = document.getElementById('root');
+ReactDOM.render(<App />, root);
+
+registerServiceWorker();
